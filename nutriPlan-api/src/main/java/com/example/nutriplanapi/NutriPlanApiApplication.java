53e0ccac0bb76900a972aca7d5b76419ce6b8bd1
fabianri2 <fabianri@hotmail.es>
2023-05-14 00:34:56 -0500

    Mensaje del commit

---
+++ nutriPlan-api/src/main/java/com/example/nutriplanapi/NutriPlanApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nutriplanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriPlanApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriPlanApiApplication.class, args);
+    }
+
+}
---
+++ nutriPlan-api/src/test/java/com/example/nutriplanapi/NutriPlanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutriplanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriPlanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
